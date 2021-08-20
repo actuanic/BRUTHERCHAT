afdd57ce2ca874a13b6e28804af7edbd7a9a0418
actuanic <62603444+actuanic@users.noreply.github.com>
2021-08-20 17:14:22 +0700

    something
    something gay

---
+++ unichat-course-master/build/precache-manifest.c198c561032d2143f6c524b409d9f0d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab5ac16599bd9930d8555734f20b9d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e2897ec613f6310de17",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "3cb73654f59833079615",
+    "url": "/static/css/main.6beb4e48.chunk.css"
+  },
+  {
+    "revision": "3e2897ec613f6310de17",
+    "url": "/static/js/2.74c8ff57.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.74c8ff57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb73654f59833079615",
+    "url": "/static/js/main.d720f8ef.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
